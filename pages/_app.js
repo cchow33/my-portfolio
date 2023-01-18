ab6c89c67a1fc23e590f182c598de8b690510cb5
Carmen Chow <carmendevelops@gmail.com>
2023-01-18 09:20:02 -0500

    theme provider dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '@/styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { ThemeProvider } from 'next-themes'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 
--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    // <Html lang="en">
+    <Html className="dark">
       <Head />
-      <body>
+      <body className="dark:bg-gray-900 dark:text-gray-100">
         <Main />
         <NextScript />
       </body>
