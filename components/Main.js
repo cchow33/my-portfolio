e46b4345497352712a80a07c66043e928213cb4d
Carmen Chow <carmendevelops@gmail.com>
2023-07-20 16:41:09 -0400

    now done done

--- components/Main.js
+++ components/Main.js
@@ -4,8 +4,8 @@ import { motion as m } from "framer-motion";
 const Main = () => {
   return (
     <main className="font-jost pt-16 p-30 flex items-center">
-      <div className="py-100 xl:max-w-7xl xl:mx-auto max-w-full pl-[5%] pr-[5%]">
-        <div className="my-30 grid h-[85%] pl-[10%] pr-[10%] items-center pt-[10%] pb-[15%] mt-[5px] text-[120px] xs:text-[34px] sm:text-[40px] xl:text-[40px] font-bold pt-[5%]">
+      <div className="py-100 xl:max-w-7xl xl:mx-auto max-w-full ">
+        <div className="my-30 grid h-[85%] items-center pt-[10%] pb-[15%] mt-[5px] text-[120px] xs:text-[34px] sm:text-[40px] xl:text-[40px] font-bold pt-[5%]">
           <m.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -18,7 +18,7 @@ const Main = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.6, duration: 5 }}
-            className="sm:text-[60px] font-jost xl:max-w-7xl xl:mx-auto max-w-full pl-[10%] pr-[10%] pt-[8%] lg:text-[80px] bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600"
+            className="sm:text-[60px] font-jost xl:max-w-7xl xl:mx-auto max-w-full pt-[8%] lg:text-[80px] bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600"
           >
             Frontend Developer
           </m.h1>
