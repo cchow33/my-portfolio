6d892f89abff290c3b59d0deec929bce7c90064e
Carmen Chow <carmendevelops@gmail.com>
2023-07-20 16:21:36 -0400

    finito

--- components/About.js
+++ components/About.js
@@ -24,14 +24,14 @@ const About = () => {
             transition={{ delay: 0.5, duration: 0.75, ease: "easeInOut" }}
             className="col-span-1 font-jost text-[20px] font-display sm:text-[18px] px-[10px] xl:text-[20px]"
           >
-            I'm a Toronto-based frontend developer interested in problem-solving
-            and building user-centred, beautiful user interfaces. As a former
-            teacher, I design and build with the end-user in mind to foster a
-            positive digital experience. I have experience working on
-            cross-functional teams that value empathy, trust and communication.
-            When I'm not bringing my ideas to life through code, I'm listening
-            to podcasts, discovering cheap eats in Toronto, and training for my
-            next 5K or 10K race.
+            I'm a Toronto-based developer interested in problem-solving and
+            building user-centred, beautiful user interfaces. Drawing on my
+            previous career in education, I approach design and code with the
+            end-user in mind, and strive to craft positive digital experiences.
+            I value being part of teams that promote empathy, trust and
+            communication. When I'm not bringing my ideas to life through code,
+            I can often be found listening to podcasts, discovering great eats
+            in Toronto, and training for my next 5K or 10K race.
           </m.p>
         </div>
       </div>
